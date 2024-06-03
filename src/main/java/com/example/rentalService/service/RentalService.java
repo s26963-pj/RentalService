a3ee309995db53d921596208a3b1b0908dce3d05
Mateusz <mateusz.szotynski3@gmail.com>
2024-06-03 23:17:17 +0200

    some changes

--- src/main/java/com/example/rentalService/service/RentalService.java
+++ src/main/java/com/example/rentalService/service/RentalService.java
@@ -1,15 +1,18 @@
 package com.example.rentalService.service;
 
+import com.example.rentalService.advice.RestTemplateResponseErrorHandler;
 import com.example.rentalService.model.Movie;
-import org.springframework.http.ResponseEntity;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 @Service
 public class RentalService {
 
-    private final RestTemplate restTemplate;
+    private RestTemplate restTemplate;
 
+    @Autowired
     public RentalService(RestTemplate restTemplate) {
         this.restTemplate = restTemplate;
     }
